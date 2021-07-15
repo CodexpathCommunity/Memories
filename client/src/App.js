9814b8db102e1e0706e9a4b585b77c99bc367813
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-07-15 02:44:50 +0100

    added the styles for all pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,21 @@ import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
+import useStyles from "./styles";
 
 function App() {
+  const classes = useStyles();
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Memories
         </Typography>
         <img
           src="https://www.vhv.rs/dpng/d/557-5570277_memories-logo-png-transparent-png.png"
           alt="memories"
-          className=""
+          height="60"
+          className={classes.image}
         />
       </AppBar>
       <Grow in>
